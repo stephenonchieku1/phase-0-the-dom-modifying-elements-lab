8fec6aeaf6a5a74db125360e96570f24ab32a135
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-11 02:55:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+
+const newHeader=document.createElement("h1");
+newHeader.setAttribute("id","victory");
+document.body.append(newHeader);
+element.append(h1);
+newHeader.textContent="YOUR-NAME is the champion";
+Element.innerHtml="is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
