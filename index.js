8852cbb19964d7683eba3fdf7f735a9b02843226
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-27 11:01:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
+
 // Write your code here!
-main .remove ();
+main.remove();
 
-
-    const newHeader = document.createElement('h1');
-    newHeader.id = 'victory';
-    newHeader.innerHTML = "YOUR-NAME is the champion";
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
 
 
 
